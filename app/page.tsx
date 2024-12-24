138bb24d39ebb4a51fe9922e0f3af455dbde980e
spikesu7 <102553793+spikesu7@users.noreply.github.com>
2024-12-24 10:34:33 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import StockTracker from '../components/StockTracker'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <StockTracker />
+    </main>
+  )
+}
+
